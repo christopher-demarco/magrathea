0830f8e9ea95b129efd0d47a4c3c469f72bb767d
Christopher DeMarco <cdemarco@gmail.com>
2017-10-25 11:06:42 -0400

    Distinguish various alphas

--- workstation/terraform.tf
+++ workstation/terraform.tf
@@ -12,7 +12,7 @@ provider "google" {
 
 resource "google_compute_instance" "workstation" {
   count = "${length(var.names)}"
-  name = "${var.names[count.index]}"
+  name = "${var.names[count.index]}${var.subdomain}"
   machine_type = "n1-standard-1"
   zone = "${var.region}-a"
   boot_disk { initialize_params { image = "ubuntu-1704" } }
