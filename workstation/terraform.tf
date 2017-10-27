be5f82900e84299049353409ecca827e3df000c4
Christopher DeMarco <cdemarco@gmail.com>
2017-10-27 15:49:05 -0400

    Port to AWS

--- workstation/terraform.tf
+++ workstation/terraform.tf
@@ -2,40 +2,64 @@ variable "admin" { default = "magrathea" }
 variable "admin_key" { default = "keys/magrathea.pub" }
 variable "names" { default = ["alpha"] }
 variable "subdomain" { default = "" }
-variable "region" { default = "us-east4" }
+variable "region" { default = "us-west-1" }
 
-provider "google" {
-  credentials = "${file("keys/magrathea.json")}"
-  project = "magrathea-178523"
+variable "ssh_key_path" {
+  default = {
+    pub = "keys/magrathea.pub"
+    priv = "keys/magrathea"
+  }
+}
+
+provider "aws" {
   region = "${var.region}"
+  profile = "magrathea"
+}
+  
+resource "aws_key_pair" "magrathea" {
+  key_name = "magrathea"
+  public_key = "${file(var.ssh_key_path["pub"])}"
 }
 
-resource "google_compute_instance" "workstation" {
-  count = "${length(var.names)}"
-  name = "${var.names[count.index]}${replace(var.subdomain, ".", "")}"
-  machine_type = "n1-standard-1"
-  zone = "${var.region}-a"
-  boot_disk { initialize_params { image = "ubuntu-1704" } }
-  network_interface { network = "default" access_config { } }
-  metadata { sshKeys = "${var.admin}:${file("${var.admin_key}")}" }
+resource "aws_instance" "magrathea" {
+  count = 1 #"${length(var.names)}"
+  ami = "ami-7f15271f"
+  instance_type = "t2.small"
+  associate_public_ip_address = true
+  key_name = "magrathea"
+  vpc_security_group_ids = ["${aws_security_group.magrathea.id}"]
+  tags { Name = "${var.names[count.index]}${replace(var.subdomain, ".", "")}" }
 }
 
-data "google_dns_managed_zone" "workstation" {
-  name = "foamninja"
+resource "aws_security_group" "magrathea" {
+  ingress {
+    from_port = 22 to_port = 22 protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }    
+  ingress {
+    from_port = 80 to_port = 80 protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0 to_port = 0 protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags { Name = "magrathea" }
 }
 
-resource "google_dns_record_set" "workstation" {
-  count = "${length(var.names)}"
-  name = "${var.names[count.index]}.${var.subdomain}${data.google_dns_managed_zone.workstation.dns_name}"
+resource "aws_route53_record" "magrathea" {
+  count = 1 #"${length(var.names)}"
+  zone_id = "Z2I8Y3PRV9V1P8"
+  name = "${var.names[count.index]}${replace(var.subdomain, ".", "")}.foam.ninja"
   type = "A"
-  ttl = 60
-  managed_zone = "${data.google_dns_managed_zone.workstation.name}"
-  rrdatas = ["${element(google_compute_instance.workstation.*.network_interface.0.access_config.0.assigned_nat_ip, count.index)}"]
+  ttl = "60"
+  records = ["${element(aws_instance.magrathea.*.public_ip, count.index)}"]
 }
 
-output "workstations" { value = "${google_compute_instance.workstation.*.network_interface.0.access_config.0.assigned_nat_ip}" }
 
-output "hostnames" { value = "${google_compute_instance.workstation.*.name}" }
+output "workstations" { value = "${aws_instance.magrathea.*.public_ip}" } 
+output "hostnames" { value = "${aws_route53_record.magrathea.*.name}" }
+
 
 # magrathea:workstation/terraform.tf
 # Copyright (c) 2017 Christopher DeMarco
